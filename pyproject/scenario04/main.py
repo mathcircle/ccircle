6c7edfee3d2007899171b915892cd54906cf551d
ccirclestudent <laalaalovesu@gmail.com>
2017-07-11 14:57:46 -0500

    Fixing main.py in scenario04

--- pyproject/scenario04/main.py
+++ pyproject/scenario04/main.py
@@ -1,12 +1,8 @@
 import ccircle
 import connection
-import random
-import time
 
 con = connection.create()
 con.send('set_name', {'name': 'put_your_name_or_nickname_here'})
-print(con.send('get_reward_ids'))
 
-for i in range(10):
-    con.send('set_velocity', {'vx': random.randint(30, 50), 'vy': random.randint(-50, 50)})
-    time.sleep(1)
+# Write code to make money and kill the evil cat!
+# See readme.txt !
